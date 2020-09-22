6771d86f5a0c170a28c21dafdf209b1d9e6bbe22
Ivan <3hitemouse@gmail.com>
2020-09-22 22:06:08 +0300

    tweaked lab 2

--- test.c
+++ test.c
@@ -4,26 +4,6 @@
 #include <math.h>
 
 
-void print_array_string(char _a []) {
-
-    int size = strlen(_a);
-
-    char * ap = (char *) malloc(size + 1);
-
-    for (int j = 0; j < size; j += 1) {
-        ap[j] = _a[j];
-    }
-    ap[size] = '\0';
-
-    for (int i = 0; i < size + 1; i += 1) {
-        printf("%c\t", ap[i]);
-    }
-
-    free(ap);
-
-}
-
-
 int main(int argc, char const *argv[]) {
 
     return 0;
