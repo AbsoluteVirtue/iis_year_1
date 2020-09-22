238192b17abf885f0e911fa69578b45faf7d2235
Ivan <3hitemouse@gmail.com>
2020-09-22 22:34:00 +0300

    test float qsort

--- test.c
+++ test.c
@@ -4,7 +4,17 @@
 #include <math.h>
 
 
-int main(int argc, char const *argv[]) {
+int _compare(const void * a, const void * b) {
 
-    return 0;
+    return (int)ceil( *(float*)a - *(float*)b );
+}
+
+
+void main(int argc, char const *argv[]) {
+
+    float array_lit [] = {67., -90., 45., 78., -13., -456., 4.};
+
+    qsort(array_lit, sizeof(array_lit) / sizeof(*array_lit), sizeof(*array_lit), _compare);
+
+    return;
 }
