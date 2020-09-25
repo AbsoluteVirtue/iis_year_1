31ee5294c56e13223c2a68a19809b8a017a7a5bb
Ivan <3hitemouse@gmail.com>
2020-09-25 04:33:06 +0300

    20200924 sem

--- lab3.c
+++ lab3.c
@@ -1,49 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NO_OF_ROWS 3
+#define ROW_LENGTH 4
+
+
+int _compare(const void * a, const void * b) {
+
+    return ( *(int*)a - *(int*)b );
+}
+
 
 int main(int argc, char const *argv[])
 {
-    
-    int array[][3] = {
-        {1, 9, -3},
-        {0, 100, 10},
-        {10, -8, 9}
+    // инициализация двумерного массива буквально
+    int array[NO_OF_ROWS][ROW_LENGTH] = {
+        {1, 3, 8, 5},
+        {3, 12, 0, 13},
+        {4, 6, 8, 14},
     };
 
-    printf("%d\n", array[0][1]);
-    printf("%d\n", *((int *)array + 0*3 + 1));
+    int array_index = 0, row_index = 1;
 
-    size_t total_size = sizeof(array);
-    size_t row_size = sizeof(*array);
-    size_t item_size = sizeof(*(int *) array);
+    // доступ к конкретному элементу по индексам ряда/элемента
+    printf("Access by index: %d\n", array[array_index][row_index]);
 
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            printf("%d\t",array[i][j]);
+    // доступ к конкретному элементу по указателю
+    printf("Access by cast pointer: %d\n", *((int *)array + array_index*ROW_LENGTH + row_index));
+
+    // вывод двумерного массива по индексам ряда/элемента
+    for (int i = 0; i < NO_OF_ROWS; ++i) {
+        for (int j = 0; j < ROW_LENGTH; ++j) {
+            printf("%d\t", array[i][j]);
         }
         printf("\n");
     }
 
-    for (int * p = (int *)array; p < (int *)array + (total_size / item_size); ++p) {
-        printf("%d\n", *p);
+    // размеры массива в памяти: целиком, одного ряда (внутреннего массива), одного элемента ряда - в байтах
+    size_t total_size = sizeof(array);
+    size_t row_size = sizeof(*array);
+    size_t item_size = sizeof(*(int *) array);
+
+    size_t array_total_items =  (total_size / item_size);
+
+    // максимальное из чисел, встречающихся в массиве более одного раза 
+    qsort(array, array_total_items, item_size, _compare);
+
+    int max_repeated_item = 0;
+
+    for (
+        int * p = (int *)array + 1;
+        p < (int *)array + array_total_items;
+        ++p
+    ) {
+
+        if (*p == *(p - 1)) max_repeated_item = *p;
+
     }
 
+    printf("Max item that repeats at least once: %d\n", max_repeated_item);
+
+    // количество строк, не содержащих ни одного нулевого элемента
     int counter = 0;
 
-    for (int i = 0; i < 3; ++i) {
-        int sum = 1;
-        for (int j = 0; j < 3; ++j) {
-            sum *= array[i][j];
-        }
-        if (sum) {
-            counter++;
-        }
-    }
-
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < NO_OF_ROWS; ++i) {
         int flag = 1;
-        for (int j = 0; j < 3; ++j) {
+        for (int j = 0; j < ROW_LENGTH; ++j) {
             if (!array[i][j]) {
                 flag = 0;
                 break;
@@ -52,7 +74,7 @@ int main(int argc, char const *argv[])
         if (flag) counter++;
     }
     
-    printf("%d", counter);
+    printf("Number or rows w/o 0: %d", counter);
 
     return 0;
 }
--- test.c
+++ test.c
@@ -1,41 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-
-
-int _compare(const void * a, const void * b) {
-
-    return ( *(int*)a - *(int*)b );
-}
 
 
 void main(int argc, char const *argv[]) {
-
-    int array[3][4] = {
-        {1, 3, 8, 5},
-        {3, 12, 2, 13},
-        {4, 6, 8, 14},
-    };
-
-    int copy [12] = {}; 
-
-    int max [] = {1, 1}, res = NULL;
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            
-            copy[i*4 + j] = array[i][j];
-
-        }
-    }
-
-    qsort(copy, 12, sizeof(*copy), _compare);
-
-    for (int i = 1; i < 12; ++i) {
-        if (copy[i] == copy[i-1]) {
-            res = copy[i];
-        }
-    }
 
     return;
 }
