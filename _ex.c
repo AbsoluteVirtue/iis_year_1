3c3acca25531b3f23bd60866025f72fe7588cf11
Ivan <3hitemouse@gmail.com>
2020-10-25 14:27:47 +0200

    att1

--- _ex.c
+++ _ex.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <iso646.h>
+#include <string.h>
+#include <time.h>
 
 
 #define N 8
@@ -58,40 +60,60 @@ void stable_partition(int* box, int size) {
 }
 
 
-int main(int argc, char const *argv[])
-{
-
-    int A[N] = {0, -2, 4, 0, -4, 3, -5, 1};
-    int B[N] = {3, -4, 5, 7, 1, -2, 4, 0};
-    int R[N + N] = {};
-    int min = INT_MAX;
-    for (int j = 0; j < N + N; j++)
+void printn(char c, int n) {
+    while ( --n >=0 ) 
     {
-        int i = 0, k = 0;
-        min = INT_MAX;
-        int flag = -1;
-        for (; i < N; i++)
-        {
-            if (A[i] < min) {
-                min = A[i];
-                k = i;
-                flag = 1;
-            } else if (B[i] < min) {
-                min = B[i];
-                k = i;
-                flag = 0;
-            }
-        }
-        if (flag != -1) {
-            if (flag) {
-                A[k] = INT_MAX;
-            } else {
-                B[k] = INT_MAX;
-            }
-        }
-        
-        R[j] = min;
+        putchar(c);
     }
+}
+
+
+void reverse(int * head, int * tail) {
+    if ( head < tail ) {
+        *head = *head + *tail;
+        *tail = *head - *tail;
+        *head = *head - *tail;
+        reverse(head + 1, tail - 1);
+    }
+}
+
+
+char* Reverse(char text[]) {
+    char* begin = text;
+    char* end = text + strlen(text) - 1;
+    char tmp;
+ 
+    for (; begin < end; ++begin, --end) {
+        tmp = *begin;
+        *begin = *end;
+        *end = tmp;
+    }
+ 
+    return text;
+}
+
+
+int main(int argc, char const *argv[]) {
+
+    srand(time(0));
+
+    int A [N] = {-3, -2, -1, 0, 4, 5, 8, 10};
+
+    char * res = Reverse("1234");
+
+    int B [N] = {3, -4, -5, -7, -10, -20, -24, -30};
+    int C [N + N] = {};
+
+    // int a = N - 1, b = 0;
+    
+    // for (int i = 0; i < N + N; ++i) {
+
+    //     if (B[b] >= A[a]) {
+    //         C[i] = B[b++];
+    //     } else if (B[b] <= A[a]) {
+    //         C[i] = A[a--];
+    //     }
+    // }
 
     return 0;
 }
