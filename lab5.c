ae46a223eaa8a44be038ecc9ed362a6c5aaa0257
Ivan <3hitemouse@gmail.com>
2020-11-28 20:02:24 +0200

    upd

--- lab5.c
+++ lab5.c
@@ -4,31 +4,28 @@
 #include <string.h>
 
 
-typedef struct _string 
+typedef struct _token 
 {
     char * str;
     size_t length;
-} string;
+} Token;
 
 int compar(const void * a, const void * b) 
 {
-    return ((string *)a)->length - ((string *)b)->length;
+    return ((Token *)a)->length - ((Token *)b)->length;
 }
 
 int main()
 { 
     char str[256] = {};
     fgets(str, 256, stdin);
-    if (str[strlen(str)] != ' ') 
-        str[strlen(str)] = ' ';
+    const char * delimiters = " ,.!?@#$%^&*();:\"\n\t";
+    Token words [10] = {};
 
-    const char * delimiters = " ,.!?@#$%^&*();:'\"\n\t";
-
-    string words [10] = {};
-
-    char *token = strtok(str, delimiters);
+    char * token = strtok(str, delimiters);
     int i = 0;
-    while(token) {
+    while(token)
+    {
         words[i].str = token;
         words[i].length = strlen(token);
         token = strtok(NULL, delimiters);
@@ -38,12 +35,16 @@ int main()
     size_t maxlen = 0;
     for (size_t i = 0; i < 10; i++)
     {
-        if(words[i].str) {
-            if (words[i].length > maxlen) maxlen = words[i].length;
+        if(words[i].str)
+        {
+            if (words[i].length > maxlen)
+            {
+                maxlen = words[i].length;
+            }
         }
     }
     
-    qsort(words, 4, sizeof(string), compar);
+    qsort(words, 4, sizeof(Token), compar);
 
     return 0;
 }
