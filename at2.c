2a39002732012bcb7b6effe65ace5962b0e6bbee
Ivan <3hitemouse@gmail.com>
2020-12-03 22:19:39 +0200

    tweaked sem 20201203

--- at2.c
+++ at2.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define N 80
+#define N 5
 
 
-void _strncpy(char * dest, const char * src, size_t count)
+char * _strncpy(char * dest, const char * src, size_t count)
 {
     size_t len = (count > strlen(src)) ? strlen(src) : count;
     for (size_t i = 0; i < len; i++)
@@ -14,11 +14,13 @@ void _strncpy(char * dest, const char * src, size_t count)
         dest[i] = src[i];
     }
     dest[len] = '\0';
+    return dest;
 }
 
 
 int _strncmp(const char * lhs, const char * rhs, size_t count)
 {
+    // TODO: строки отличаются, если они разной длины, и count больше обеих
     size_t len = (strlen(rhs) > strlen(lhs)) ? strlen(lhs) : strlen(rhs);
     size_t k = (count > len) ? len : count;
     for (size_t i = 0; i < k; i++)
@@ -32,19 +34,26 @@ int _strncmp(const char * lhs, const char * rhs, size_t count)
 }
 
 
-int _atoi(const char * src) 
+int _atoi(const char * str) 
 {
+    // TODO: добавить определение отрицательных чисел
     int res = 0;
-    char buf [80] = {};
-    size_t i = 0, k = 0;
-    while (src[i])
+    char buf [strlen(str)];
+    size_t i = 0, k = 0, stop = 0;
+    while (str[i])
     {
-        if(isdigit(src[i])) {
-            buf[k++] = src[i];
-        }
+        stop = k;
+        if (isdigit(str[i]))
+            buf[k++] = str[i];
+        if (k && stop == k)
+            break;
         ++i;
     }
-    sscanf(buf, "%i", &res);
+    if (k) 
+    {
+        buf[k] = '\0';
+        sscanf(buf, "%i", &res);
+    }
     return res;
 }
 
@@ -53,10 +62,12 @@ void bytewise_inc(unsigned short * src, size_t count)
 {
     for (size_t i = count - 1; i >= 0; i--)
     {
-        if (src[i]) 
+        if (src[i])
         {
             src[i] = 0;
-        } else {
+        } 
+        else
+        {
             src[i] = 1;
             return;
         }
@@ -66,22 +77,43 @@ void bytewise_inc(unsigned short * src, size_t count)
 
 int main(int argc, char const *argv[])
 {
-    // " -123junk"  -123
-    // "junk"       0
-    // "-junk123"   123
-    const char * test = "Happy New Year 2021!";
-    int r = _atoi(test);
-    printf("%d", r);
+    const char * s1 = "Happy New Year 2021!";
+    char * s2 = (char *)malloc(strlen(s1) + 1);
+    _strncpy(s2, s1, 19);
+    printf("Copied string: %s\n", s2);
 
-    // 128 64 32 16 8 4 2 1
+    ////////////////////////////////
 
-    //   7  6  5  4 3 2 1 0
+    printf("Comparison result: %d\n", _strncmp(s1, s2, 27));
 
-    //   1  0  1  0 1 1 1 1     175
-    //   0  0  0  1 0 1 0 0     20
+    ////////////////////////////////
 
-    unsigned short b [] = {0, 0,0, 1, 0, 0,1,1};
+    /*
+        конвертирование строк в числа
 
+                        ожидаемый
+        ввод:           результат:
+        " -123junk"     -123
+        "junk"          0
+        "-junk123"      123
+    */
+    const char * val = "-312junk123";
+    printf("Converted value from %s: %d\n", val, _atoi(val));
+
+    ////////////////////////////////
+
+    /*
+        десятичное представление двоичных чисел
+
+          7  6  5  4 3 2 1 0    (степень 2-ки)
+        128 64 32 16 8 4 2 1    (десятичное представление степени двойки в бите)
+
+        1  0  1  0 1 1 1 1      175 (десятичная сумма битов, выставленных в 1)
+        0  0  0  1 0 1 0 0      20
+    */
+    printf("Incremented value: ");
+
+    unsigned short b [] = {0, 0, 0, 1, 0, 0, 1, 1};
     bytewise_inc(b, 8);
     for (size_t i = 0; i < 8; i++)
     {
@@ -90,22 +122,26 @@ int main(int argc, char const *argv[])
 
     printf("\n");
 
+    ////////////////////////////////
+
+    /*
+        четверти квадратной матрицы, включая диагонали
+
+        \ 1 /
+         \ /
+       4 / \ 2
+        / 3 \
+
+        для четвертой четверти:
+    */
     int m [N][N] = {
         {1, 2, 3, 4, 9},
         {5, 6, 7, 8, 1},
         {1, 2, 3, 4, 8},
         {5, 6, 7, 8, 0},
         {5, 6, 7, 8, 1},
-    };
+    }, sum = 0;
 
-    /*
-        \ 1 /
-         \ /
-        4/ \2
-        / 3 \
-    */
-
-    int sum = 0;
     for (size_t i = 0; i < (N / 2) + 1; i++)
     {
         for (size_t j = 0; j < N; j++)
@@ -113,10 +149,10 @@ int main(int argc, char const *argv[])
             if (j <= N - 1 - i && j >= i) 
             {
                 sum += m[j][i];
-                printf("%d", m[j][i]);
             }
         }
     }
+    printf("Sum for given quarter: %d\n", sum);
 
-    printf("\n%d", sum);
+    return 0;
 }
