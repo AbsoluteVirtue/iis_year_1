aab0e31182b1bde611856aeedb486b07447d9a75
Leon <leonbrcs@gmail.com>
2020-12-05 16:52:15 +0200

    new ex

--- _ex.c
+++ _ex.c
@@ -1,44 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <iso646.h>
 #include <string.h>
-#include <time.h>
-#include <stdbool.h>
-#include <ctype.h>
 
-#define N 5
+typedef unsigned short uint8;
 
-
-int _strncmp(const char * lhs, const char * rhs, size_t count)
+struct book
 {
+    char * title;
+    char * author;
+    uint8 year;
+};
 
+
+void init(struct book * bp)
+{
+    bp->author = NULL;
+    bp->title = NULL;
+    bp->year = 0;
 }
 
 
-void _strncpy(char * dest, const char * src, size_t count)
+void fill(  struct book * bp, 
+            const char * a, 
+            const char * t, 
+            uint8 y) 
 {
-
+    bp->author = (char *)malloc(strlen(a) + 1);
+    if(bp->author) 
+    {
+        strcpy(bp->author, a);
+    }
+    bp->title = (char *)malloc(strlen(t) + 1);
+    if(bp->title) 
+    {
+        strcpy(bp->title, t);
+    }
+    bp->year = y;
 }
 
 
-
-int _atoi(const char * src) 
+void clear(struct book * bp) 
 {
-
+    bp->year = 0;
+    if(bp->author) 
+    {
+        free(bp->author);
+        bp->author = NULL;
+    }
+    if(bp->title) 
+    {
+        free(bp->title);
+        bp->title = NULL;
+    }
 }
 
 
-
-void bytewise_inc(unsigned short * src, size_t count) 
+int comp(const void * a, const void * b) 
 {
-
+    return (((struct book *)a)->year) - (((struct book *)b)->year);
 }
 
 
-int main(int argc, char const *argv[]) 
+int main(int argc, char const *argv[])
 {
+    struct book b1, b2;
+    init(&b1);
+    init(&b2);
 
+    fill(&b2, "Алексей Толстой", "Князь Серебряный", 1900);
+    fill(&b1, "Владимир Маяковский", "Облако в штанах", 1920);
 
-    return 0;
+    struct book * sob = (struct book *)malloc(
+        2 * sizeof(struct book));
+
+    sob[0] = b1;
+    sob[1] = b2;
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        printf( "%s \"%s\" (%d)\n", 
+                sob[i].author, sob[i].title, sob[i].year);
+    }
+
+    qsort(sob, 2, sizeof(struct book), comp);
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        printf( "%s \"%s\" (%d)\n", 
+                sob[i].author, sob[i].title, sob[i].year);
+        clear(&sob[i]);
+    }
+
+    free(sob);
 }
